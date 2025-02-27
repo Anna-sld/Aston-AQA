af88d14a0e67cddbe0619e24d4c4257c764d6b6e
Anna <solodkova.ania@gmail.com>
2025-02-27 14:49:12 +0200

    created a method printThreeWords

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
-
-
+        printThreeWords();
     }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
 }
